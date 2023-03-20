56b55cabd439a6482a062af26013ae22f4ae4bda
Viktoriya <vajsmanv@bk.ru>
2023-03-20 12:23:37 +0300

    x

---
+++ src/main/java/web/pp311/dao/UserDaoImpl.java
@@ -0,0 +1,43 @@
+package web.pp311.dao;
+
+import org.springframework.stereotype.Repository;
+import web.pp311.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+@Repository
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> allUsers() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        entityManager.createQuery("delete from User where id=: id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    @Override
+    public void edit(User user, int id) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
